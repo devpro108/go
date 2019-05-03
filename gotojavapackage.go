f6d6d1dcb2fc1b206043b5d3d542aa08187eceae
Dev Pro (^ , ^) <47504653+devpro108@users.noreply.github.com>
2019-05-03 16:52:42 -0400

    Updated to add Java to Go package conversion

--- gotojavapackage.go
+++ gotojavapackage.go
@@ -7,14 +7,21 @@ import (
 
 func main() {
 
-	goPkgName := "github.com/golang/packages"
+	goPkgName := "github.com/devpro108/go"
 	
 	javaPkgName := GoToJavaPackage(goPkgName)
 	
 	fmt.Println(goPkgName)
 	fmt.Println(javaPkgName)
 	
+	goPkgName = ""
+
+	goPkgName = JavaToGoPackage(javaPkgName)
+
+	fmt.Println(goPkgName)
+	
 }
+
 
 /* 
   Function converts go package to java package by performing reverse domain and / to . converstion 
@@ -30,9 +37,35 @@ func GoToJavaPackage(goPkg string) (javaPkg string) {
 	
 		slashIdx = strings.Index(goPkg, "/")
 	
-		if dotIdx < slashIdx {
+		if ( slashIdx - dotIdx ) > 1 {
 			javaPkg = goPkg[dotIdx + 1 :slashIdx] + "." + goPkg[:dotIdx] + goPkg[slashIdx:]
 			javaPkg = strings.Replace(javaPkg , "/", ".", -1)
+		}		
+	
+	}
+
+	return
+}
+
+/* 
+  Function converts java package to go package by adjusting domain name and . to / converstion 
+  Assumes java package starts with domain name, in other cases, it returns blank string
+*/
+func JavaToGoPackage(javaPkg string) (goPkg string) {
+	
+	var dotIdx1, dotIdx2 int
+	
+	dotIdx1 = strings.Index(javaPkg, ".")
+	
+	if dotIdx1 > 1 {
+	
+		dotIdx2 = strings.Index(javaPkg[dotIdx1 + 1:], ".")
+		dotIdx2 = dotIdx2 + dotIdx1 + 1 
+			
+		if ( dotIdx2 - dotIdx1 ) > 1 {
+		
+			goPkg = strings.Replace(javaPkg[dotIdx2:], ".", "/", -1)
+			goPkg = javaPkg[dotIdx1 + 1 :dotIdx2] + "." + javaPkg[:dotIdx1] + goPkg
 		}		
 	
 	}
